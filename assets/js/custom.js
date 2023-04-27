a76b62d3a12f7f0714b400d04c00d7dbffa688cc
ShehrozEdu <battshehrozedu@gmail.com>
2023-04-27 14:54:22 +0530

    pdf logic done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,14 +3,14 @@ var downloadButton = document.querySelector(".button-download");
 downloadButton.addEventListener("click", function () {
   var activeItem = document.querySelector(".owl-resume-item .owl-item.active");
 
-  var pdfElement = activeItem.querySelector("object"); // Assuming the PDF is embedded using <object> tag
+  var docElement = activeItem.querySelector("object"); // Assuming the Word document is embedded using <object> tag
 
-  // Extract the PDF source URL
-  var pdfURL = pdfElement.getAttribute("data");
-
+  // Extract the document source URL
+  var docURL = docElement.getAttribute("data");
+  const randomNumber = Math.floor(Math.random() * 10000) + 1;
   var downloadLink = document.createElement("a");
-  downloadLink.href = pdfURL;
-  downloadLink.download = "resume-template.pdf";
+  downloadLink.href = docURL;
+  downloadLink.download = `"resume-template-${randomNumber}.doc"`;
   downloadLink.style.display = "none";
   document.body.appendChild(downloadLink);
 
